478ea6a4fb9d6862b8e7173b4c0e0c2895124a81
Philip Garip <phil.garip@gmail.com>
2021-05-25 20:46:43 -0400

    added index api routes

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const userRoutes = require('./api/userRoutes');
+const postRoutes = require('./api/postRoutes');
+const commentRoutes = require('./api/commentRoutes');
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
